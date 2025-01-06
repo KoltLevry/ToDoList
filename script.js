d940f9b534819ee54dde5de2a9aa34476c74be34
volkde <ilyaprofit@gmail.com>
2025-01-06 14:30:21 +0100

    bug fix1

--- script.js
+++ script.js
@@ -1,19 +1,21 @@
 const taskInput = document.querySelector('.input-container input[type="text"]');
-const addTaskBtn = document.querySelector('.input-container button');
+const addTaskBtn = document.getElementById('addTask');
 const taskList = document.querySelector('ol');
 const deleteListBtn = document.getElementById('deleteList');
 
-addTaskBtn.addEventListener('click', function() {
-    const taskText = taskInput.value.trim();
 
-    if (taskText !== '') {
-        const listItem = document.createElement('li');
-        listItem.textContent = taskText;
+addTaskBtn.addEventListener('click', function () {
+    const taskText = taskInput.value.trim(); 
+
+    if (taskText !== '') { 
+        const listItem = document.createElement('li'); 
+        listItem.textContent = taskText; 
         taskList.appendChild(listItem);
         taskInput.value = ''; 
     }
 });
 
-deleteListBtn.addEventListener('click', function() {
-    taskList.innerHTML = '';
+
+deleteListBtn.addEventListener('click', function () {
+    taskList.innerHTML = ''; 
 });

a8c51167e2367a24f08ad84afa1fe593db4b3af2
Kristina Tereshchenko <kristovinia@gmail.com>
2025-01-06 14:05:24 +0100

    js

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const taskInput = document.querySelector('.input-container input[type="text"]');
 const addTaskBtn = document.querySelector('.input-container button');
 const taskList = document.querySelector('ol');
+const deleteListBtn = document.getElementById('deleteList');
 
 addTaskBtn.addEventListener('click', function() {
     const taskText = taskInput.value.trim();
@@ -11,4 +12,8 @@ addTaskBtn.addEventListener('click', function() {
         taskList.appendChild(listItem);
         taskInput.value = ''; 
     }
+});
+
+deleteListBtn.addEventListener('click', function() {
+    taskList.innerHTML = '';
 });
